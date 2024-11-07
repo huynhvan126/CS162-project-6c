f4d4c1b226d9fd14c298316e8ed6adbe629b72b5
huynhvan126 <165982795+huynhvan126@users.noreply.github.com>
2024-11-06 16:22:20 -0800

    Add files via upload

---
+++ is_subsequence.py
@@ -0,0 +1,18 @@
+# Author: Van Huynh
+# GitHub username: huynhvan126
+# Date: 11/06/2024
+# Description: Write a recursive function that takes two string parameters and returns True if the first string is a subsequence of the second string, but returns False otherwise.
+def is_subsequence(str1, str2):
+    """
+    This function checks whether str1 is a subsequence of str2.
+    """
+    if not str1:
+        return True
+    if not str2:
+        return False
+
+    # Check if 1st characters match. If yes, continue searching in the remaining str2.
+    if str1[0] == str2[0]:
+        return is_subsequence(str1[1:], str2[1:])
+    # If 1st character don't match, continue searching
+    return is_subsequence(str1, str2[1:])
